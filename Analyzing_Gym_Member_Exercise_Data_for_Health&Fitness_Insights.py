a12d8c364b622b60a3433eb5039aced17eb6dcfa
Akanksha Singh <156396471+Akku-1206@users.noreply.github.com>
2024-11-12 13:58:39 +0530

    Update Analyzing_Gym_Member_Exercise_Data_for_Health&Fitness_Insights.py

--- Analyzing_Gym_Member_Exercise_Data_for_Health&Fitness_Insights.py
+++ Analyzing_Gym_Member_Exercise_Data_for_Health&Fitness_Insights.py
@@ -1,10 +1,9 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 from sklearn.preprocessing import StandardScaler
 
